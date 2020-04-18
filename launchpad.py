276e03b432f0d05ad020ef1969a4d54c329b1344
Simone Cosimo <simosx4@gmail.com>
2020-04-18 22:25:33 +0200

    Splitted Launch class in other file

--- launchpad.py
+++ launchpad.py
@@ -7,37 +7,7 @@ import requests
 import json
 
 from creds import token as bot_token
-
-class Launch():
-    def __init__(self, mission_name, provider, vehicle, padname, padloc, description, launch_date):
-        self.gmpas_baseurl = "https://www.google.com/maps/search/?api=1&query="
-        self.mission_name = mission_name
-        self.provider = provider
-        self.vehicle = vehicle
-        self.padname = padname
-        self.padloc = padloc
-        self.description = description
-        self.text = self.createDisplayText()
-        self.date = "TBD"
-
-        #if launch_date is not "None":
-        #    parts = launch_date.split('T')
-        #    day = parts[0].split('-')
-        #    self.date = day[1] + '-' + day[2] + '-' + day[0] + ' ' + parts[1][0:-1] + '(UTC)'
-
-    def createDisplayText(self):
-        print(self.date)
-        gmaps_querykey = self.padname.replace(' ', '+')
-        text = '<b>Mission:</b> ' + self.mission_name + '\n' \
-                '<b>Provider:</b> ' + self.provider + '\n' \
-                '<b>Vehicle:</b> ' + self.vehicle + '\n' \
-                '<b>Launching from</b> <a href="' + self.gmpas_baseurl + gmaps_querykey + '">' + self.padname + ' (' + self.padloc + ')</a>\n' \
-                '<b>Date:</b> <i>' + self.date + '</i>\n\n' \
-                '' + self.description
-        return text
-    
-    def getFormattedText(self):
-        return self.text
+from launch import Launch
 
 #Start command
 def start(update, context):
