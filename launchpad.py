ac9fdc96d78a9122e64da02c2710c3e496e4a8fe
Simone Cosimo <simosx4@gmail.com>
2020-04-18 23:59:01 +0200

    Testing pic sending with caption

--- launchpad.py
+++ launchpad.py
@@ -58,7 +58,7 @@ def nextLaunch(update, context):
         if str(api_json[i]['pad']['location']['statename']) == "None":
             padlocation = str(api_json[i]['pad']['location']['country'])
         print(padlocation)
-        
+
         l = Launch(
             str(api_json[i]['name']),
             str(api_json[i]['provider']['name']),
@@ -72,6 +72,44 @@ def nextLaunch(update, context):
     
     context.bot.send_message(chat_id=update.effective_chat.id, 
                             text=txts[0], 
+                            parse_mode=ParseMode.HTML)
+
+def nextPhotoTest(update, context):
+    j = requests.get('https://fdo.rocketlaunch.live/json/launches/next/5')
+    n_results = 1
+    api_json = json.loads(j.text)['result']
+    txts = []
+    for i in range(0, n_results):
+        padlocation = str(api_json[i]['pad']['location']['statename']) + ', ' + str(api_json[i]['pad']['location']['country'])
+        print(str(api_json[i]['pad']['location']['statename']))
+        if str(api_json[i]['pad']['location']['statename']) == "None":
+            padlocation = str(api_json[i]['pad']['location']['country'])
+        print(padlocation)
+
+        l = Launch(
+            str(api_json[i]['name']),
+            str(api_json[i]['provider']['name']),
+            str(api_json[i]['vehicle']['name']),
+            str(api_json[i]['pad']['location']['name']),
+            padlocation,
+            str(api_json[i]['launch_description']),
+            str(api_json[i]['win_open'])
+        )
+        txts.append(l.getFormattedText())
+    
+    context.bot.send_photo(chat_id=update.effective_chat.id, 
+                            photo=open('falcon9.jpg', 'rb'),
+                            caption=txts[0], 
+                            parse_mode=ParseMode.HTML)
+
+
+def helpCommand(update, context):
+    helptext = "<b>/start</b> - Welcomes you\n" \
+                "<b>/launches</b> - Shows the upcoming 5 launches\n" \
+                "<b>/next</b> - Shows the next upcming launch\n" \
+                "<b>/help</b> - Shows this help screen\n"
+    context.bot.send_message(chat_id=update.effective_chat.id, 
+                            text=helptext, 
                             parse_mode=ParseMode.HTML)
 
 def main():
@@ -91,6 +129,12 @@ def main():
 
     nextOne_handler = CommandHandler('next', nextLaunch)
     dispatcher.add_handler(nextOne_handler)
+
+    nextPic_handler = CommandHandler('nextpic', nextPhotoTest)
+    dispatcher.add_handler(nextPic_handler)
+
+    help_handler = CommandHandler('help', helpCommand)
+    dispatcher.add_handler(help_handler)
 
     # Start polling for commands
     updater.start_polling()
