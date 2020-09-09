f24cc10733c495bd48d18aa4b5ea875b339ae802
Simone Cosimo <simosx4@gmail.com>
2020-09-09 23:07:57 +0200

    fresh start lol

--- launch.py
+++ launch.py
@@ -1,5 +1,9 @@
+from emoji import emojize
+
+rocket = emojize(":rocket:", use_aliases=True)
+
 class Launch():
-    def __init__(self, mission_name, provider, vehicle, padname, padloc, description, launch_date):
+    def __init__(self, mission_name, provider, vehicle, padname, padloc, description, launch_date, livestream):
         self.gmpas_baseurl = "https://www.google.com/maps/search/?api=1&query="
         self.mission_name = mission_name
         self.provider = provider
@@ -7,16 +11,22 @@ class Launch():
         self.padname = padname
         self.padloc = padloc
         self.description = description
+        #self.livestream = livestream
         
         self.date = "TBD"
         print("Date parameter: " + launch_date + '\n')
         if launch_date != "None":
-            parts = launch_date.split('T')
-            day = parts[0].split('-')
-            print('Parts: ' + str(len(parts)) + '\nDay: ' + str(len(day)))
-            self.date = day[1] + '-' + day[2] + '-' + day[0] + ' ' + parts[1][0:-1] + '(UTC)'
+            if launch_date[-1:] == 'Z':
+                parts = launch_date.split('T')
+                day = parts[0].split('-')
+                print('Parts: ' + str(len(parts)) + '\nDay: ' + str(len(day)))
+                self.date = day[1] + '-' + day[2] + '-' + day[0] + ' ' + parts[1][0:-1] + '(UTC)'
+            else:
+                self.date = launch_date
 
         self.text = self.createDisplayText()
+        if self.description == "nodescforlist":
+            self.text = self.createListText()
 
     def createDisplayText(self):
         print(self.date)
@@ -26,7 +36,22 @@ class Launch():
                 '<b>Vehicle:</b> ' + self.vehicle + '\n' \
                 '<b>Launching from</b> <a href="' + self.gmpas_baseurl + gmaps_querykey + '">' + self.padname + ' (' + self.padloc + ')</a>\n' \
                 '<b>Date:</b> <i>' + self.date + '</i>\n\n' \
-                '' + self.description
+        
+        if self.description != "nodescforlist":
+            text = text + '' + self.description
+        
+        #if self.livestream != "":
+            #text = text + '\n<b>Follow the <a href="' + self.livestream + '">Livestream</a> <i>(if available)</i></b>'
+
+        return text
+
+    def createListText(self):
+        print(self.date)
+        #gmaps_querykey = self.padname.replace(' ', '+')
+        text = '<b>Provider:</b> ' + self.provider + '\n' \
+                '<b>Vehicle:</b> ' + self.vehicle + '\n' \
+                '<b>Date:</b> <i>' + self.date + '</i>\n\n' \
+        
         return text
     
     def getFormattedText(self):
--- launchpad.py
+++ launchpad.py
@@ -1,6 +1,7 @@
 from telegram.ext import Updater
 from telegram.ext import CommandHandler, MessageHandler
 from telegram import ParseMode, ChatAction
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 
 import logging
 import requests
@@ -8,7 +9,7 @@ import json
 import os
 
 #from creds import token as bot_token
-from launch import Launch
+from launch import Launch, rocket
 
 from functools import wraps
 
@@ -39,6 +40,7 @@ def launches(update, context):
     n_results = 5
     api_json = json.loads(j.text)['result']
     txts = []
+    livestream = ""
     for i in range(0, n_results):
         padlocation = str(api_json[i]['pad']['location']['statename']) + ', ' + str(api_json[i]['pad']['location']['country'])
         print(str(api_json[i]['pad']['location']['statename']))
@@ -46,25 +48,58 @@ def launches(update, context):
             padlocation = str(api_json[i]['pad']['location']['country'])
         print(padlocation)
 
+        launch_date = str(api_json[i]['win_open'])
+        if launch_date == "None":
+            if str(api_json[i]['est_date']['month']) != "None":
+                day = '0'
+                month = '0'
+                if int(api_json[i]['est_date']['month']) < 10:
+                    month = month + str(api_json[i]['est_date']['month'])
+                else:
+                    month = str(api_json[i]['est_date']['month'])
+
+                if str(api_json[i]['est_date']['day']) != "None":
+                    if int(api_json[i]['est_date']['day']) < 10:
+                        day = day + str(api_json[i]['est_date']['day'])
+                    else:
+                        day = str(api_json[i]['est_date']['day'])
+                else:
+                    day = "??"
+
+                launch_date = month + '-' + day + '-' + str(api_json[i]['est_date']['year'])
+
         l = Launch(
             str(api_json[i]['name']),
             str(api_json[i]['provider']['name']),
             str(api_json[i]['vehicle']['name']),
             str(api_json[i]['pad']['location']['name']),
             padlocation,
-            str(api_json[i]['launch_description']),
-            str(api_json[i]['win_open'])
+            'nodescforlist',
+            launch_date,
+            livestream
         )
-        txts.append(l.getFormattedText())
+        txts.append(rocket + ' #' + str(i+1) + ' - <i>' + str(api_json[i]['name']) + '</i> ' + rocket \
+                    + '\n' + l.getFormattedText())
     
     endtext = ""
     for i in range(0, len(txts)):
-        endtext = endtext + txts[i] + '\n===========\n'
+        endtext = endtext + txts[i]
     
     context.bot.send_message(chat_id=update.effective_chat.id, 
                             text=endtext, 
                             disable_web_page_preview=True,
                             parse_mode=ParseMode.HTML)
+
+def build_menu(buttons,
+               n_cols,
+               header_buttons=None,
+               footer_buttons=None):
+    menu = [buttons[i:i + n_cols] for i in range(0, len(buttons), n_cols)]
+    if header_buttons:
+        menu.insert(0, [header_buttons])
+    if footer_buttons:
+        menu.append([footer_buttons])
+    return menu
 
 @send_typing_action
 def nextLaunch(update, context):
@@ -73,6 +108,7 @@ def nextLaunch(update, context):
     n_results = 1
     api_json = json.loads(j.text)['result']
     txts = []
+    livestream = ""
     for i in range(0, n_results):
         padlocation = str(api_json[i]['pad']['location']['statename']) + ', ' + str(api_json[i]['pad']['location']['country'])
         print(str(api_json[i]['pad']['location']['statename']))
@@ -80,6 +116,33 @@ def nextLaunch(update, context):
             padlocation = str(api_json[i]['pad']['location']['country'])
         print(padlocation)
 
+        launch_date = str(api_json[i]['win_open'])
+        if launch_date == "None":
+            if str(api_json[i]['est_date']['month']) != "None":
+                day = '0'
+                month = '0'
+                if int(api_json[i]['est_date']['month']) < 10:
+                    month = month + str(api_json[i]['est_date']['month'])
+                else:
+                    month = str(api_json[i]['est_date']['month'])
+
+                if str(api_json[i]['est_date']['day']) != "None":
+                    if int(api_json[i]['est_date']['day']) < 10:
+                        day = day + str(api_json[i]['est_date']['day'])
+                    else:
+                        day = str(api_json[i]['est_date']['day'])
+                else:
+                    day = "??"
+
+                launch_date = month + '-' + day + '-' + str(api_json[i]['est_date']['year'])
+
+        if str(api_json[i]['quicktext']) != "None":
+            parts = str(api_json[i]['quicktext']).split('- ')
+            for x in parts:
+                if "https" in x:
+                    livestream = x
+
+        print("Livestream link: " + livestream)
         l = Launch(
             str(api_json[i]['name']),
             str(api_json[i]['provider']['name']),
@@ -87,9 +150,11 @@ def nextLaunch(update, context):
             str(api_json[i]['pad']['location']['name']),
             padlocation,
             str(api_json[i]['launch_description']),
-            str(api_json[i]['win_open'])
+            launch_date,
+            livestream
         )
-        txts.append(l.getFormattedText())
+        txts.append(rocket + ' #' + str(i+1) + ' - <i>' + str(api_json[i]['name']) + '</i> ' + rocket \
+                    + '\n' + l.getFormattedText())
     
     providers_list = ["spacex", "ula", "nasa", "roscosmos", "jaxa", "china"]
     selected = ""
@@ -100,21 +165,27 @@ def nextLaunch(update, context):
             selected = providers_list[i] + '.jpg'
             break
 
+    button_list = [
+        InlineKeyboardButton("Watch Livestream (if available)", url=livestream)
+    ]
+    reply_markup = InlineKeyboardMarkup(build_menu(button_list, n_cols=1))
+
     context.bot.send_photo(chat_id=update.effective_chat.id, 
                             photo=open('imgs/' + selected, 'rb'),
                             caption=txts[0], 
-                            parse_mode=ParseMode.HTML)
+                            parse_mode=ParseMode.HTML,
+                            reply_markup=reply_markup)
 
 @send_typing_action
 def info(update, context):
     pass
     text = "<b>Launchpad Bot</b> for Telegram by @simocosimo\n\n" \
-            "I really hope you like the bot. It is a work in progress, I want to improve" \
+            "Hi! I really hope you're liking the bot. It is a work in progress, I want to improve" \
             " it as much as possible, in style and in informations. Bare in mind that " \
             " space launches are usually subject to change in schedule, so take the dates" \
-            " with a grain of salt. \nIf you want to make a little donation to help the " \
-            "project, <a href='https://www.paypal.me/simocosimo'>you can do it!</a> You absolutely don't have to, but they are really " \
-            "appreciated!"
+            " with a grain of salt. \nFeel free to tweet me at <a href='https://twitter.com/simocosimo'>@simocosimo</a> for problems" \
+            " or to ask for a particular function that, according to you, could be useful!\nLet's hope" \
+            " we'll not have problems, Houston!"
     context.bot.send_message(chat_id=update.effective_chat.id, 
                             text=text, 
                             parse_mode=ParseMode.HTML)
@@ -144,8 +215,8 @@ def main():
                         level=logging.INFO)
     #logger = logging.getLogger(__name__)
 
-    start_handler = CommandHandler('start', start)
-    dispatcher.add_handler(start_handler)
+    #start_handler = CommandHandler('start', start)
+    #dispatcher.add_handler(start_handler)
 
     nextFive_handler = CommandHandler('launches', launches)
     dispatcher.add_handler(nextFive_handler)
@@ -159,8 +230,8 @@ def main():
     info_handler = CommandHandler('info', info)
     dispatcher.add_handler(info_handler)
 
-    help_handler = CommandHandler('help', helpCommand)
-    dispatcher.add_handler(help_handler)
+    #help_handler = CommandHandler('help', helpCommand)
+    #dispatcher.add_handler(help_handler)
 
     # Start the webhook
     updater.start_webhook(listen="0.0.0.0",
